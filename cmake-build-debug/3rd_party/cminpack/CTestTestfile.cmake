34633de4af91ee9a4ce1be6d95c87eee50624491
leokan <leo.kan01@gmail.com>
2022-05-20 14:00:59 +0200

    initial commit

---
+++ cmake-build-debug/3rd_party/cminpack/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/leokan/Documents/TUDelft/GEO1016-3DV/A2_Triangulation/A2_Triangulation_Code/3rd_party/cminpack
+# Build directory: /Users/leokan/Documents/TUDelft/GEO1016-3DV/A2_Triangulation/A2_Triangulation_Code/cmake-build-debug/3rd_party/cminpack
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("cmake")
